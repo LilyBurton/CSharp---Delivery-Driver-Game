68c6f4201e850e1576e4b912d2d63f32f0b61b4a
LilyBurton <lil@192.168.1.168>
2023-12-07 17:16:09 +0000

    Put steering and speed onto the car

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 1;
+    [SerializeField] float moveSpeed = 0.01f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
