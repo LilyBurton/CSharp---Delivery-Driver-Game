384eee81a9f915605ad5f0deb6dac4c13d1c1777
LilyBurton <lil@192.168.1.185>
2024-01-25 17:25:28 +0000

    Change color of car when package is picked up, makes the car slower when bumped into things and makes car faster w hen going over the boost.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1;
-    [SerializeField] float moveSpeed = 0.01f;
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed = 20f;
 
+    [SerializeField] float slowSpeed = 15f;
 
+    [SerializeField] float boostSpeed = 30f;
+
+    BoxCollider2D boxCollider;
+   
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float steerAccelerate = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, steerAccelerate, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+
+            moveSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "boost") {
+            Debug.Log("Got to go fast!");
+            moveSpeed = boostSpeed;
+        }
     }
 }
